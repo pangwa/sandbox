3197829634cc49b8a053ca8bef3caa3269ea0f74
pangwa <pangwa@gmail.com>
2011-09-28 07:07:04 +0800

    implementing collada file loader

---
+++ webgl/scripts/collada-loader.js
@@ -0,0 +1,9 @@
+var PhiloGL.IO.Collada_loader = function ()
+{
+    var parse = function (xmlDoc, callback)
+    {
+        $(xmlDoc).find ('instance_geometry').each (function ()
+            {
+            });
+};
+
